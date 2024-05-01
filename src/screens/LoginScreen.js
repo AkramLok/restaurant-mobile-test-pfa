afc31b3e8e9382104cf6a78559941bf1108f46eb
AkramLok <akram15191519@gmail.com>
2024-05-01 14:25:18 +0100

    Changes user role

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,33 +1,60 @@
-import React, { useState } from 'react'
-import { TouchableOpacity, StyleSheet, View } from 'react-native'
-import { Text } from 'react-native-paper'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import Button from '../components/Button'
-import TextInput from '../components/TextInput'
-import BackButton from '../components/BackButton'
-import { theme } from '../core/theme'
-import { emailValidator } from '../helpers/emailValidator'
-import { passwordValidator } from '../helpers/passwordValidator'
+import React, { useState } from "react";
+import { TouchableOpacity, StyleSheet, View, Alert } from "react-native";
+import { Text } from "react-native-paper";
+import Background from "../components/Background";
+import Logo from "../components/Logo";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import TextInput from "../components/TextInput";
+import BackButton from "../components/BackButton";
+import { theme } from "../core/theme";
+import { emailValidator } from "../helpers/emailValidator";
+import { passwordValidator } from "../helpers/passwordValidator";
+import AuthService from "../services/auth.service"; // Import AuthService
 
 export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = useState({ value: '', error: '' })
-  const [password, setPassword] = useState({ value: '', error: '' })
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
-  const onLoginPressed = () => {
-    const emailError = emailValidator(email.value)
-    const passwordError = passwordValidator(password.value)
-    if (emailError || passwordError) {
-      setEmail({ ...email, error: emailError })
-      setPassword({ ...password, error: passwordError })
-      return
+  const onLoginPressed = async () => {
+    // const usernameError = emailValidator(username);
+    // const passwordError = passwordValidator(password);
+    // if (usernameError || passwordError) {
+    //   Alert.alert("Validation Error", usernameError || passwordError);
+    //   return;
+    // }
+
+    setLoading(true);
+
+    try {
+      const response = await AuthService.login(username, password);
+      // Assuming the login response contains a username upon successful login
+      if (response && response.username) {
+        setMessage("Registration successful");
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "Dashboard" }],
+        });
+      } else {
+        Alert.alert("Login Failed", "Invalid username or password");
+      }
+    } catch (error) {
+      const errorMessage =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      // Handle registration error
+      setMessage(errorMessage);
+      console.error("Login Error:", errorMessage);
+      //Alert.alert("Login Error", "Failed to log in. Please try again.");
     }
-    navigation.reset({
-      index: 0,
-      routes: [{ name: 'Dashboard' }],
-    })
-  }
+
+    setLoading(false);
+  };
 
   return (
     <Background>
@@ -35,56 +62,52 @@ export default function LoginScreen({ navigation }) {
       <Logo />
       <Header>Hello.</Header>
       <TextInput
-        label="Email"
+        label="Username"
         returnKeyType="next"
-        value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
-        error={!!email.error}
-        errorText={email.error}
-        autoCapitalize="none"
-        autoCompleteType="email"
-        textContentType="emailAddress"
-        keyboardType="email-address"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+        error={false} // No need for error state for username in this case
       />
       <TextInput
         label="Password"
         returnKeyType="done"
-        value={password.value}
-        onChangeText={(text) => setPassword({ value: text, error: '' })}
-        error={!!password.error}
-        errorText={password.error}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        error={false} // No need for error state for password in this case
         secureTextEntry
       />
       <View style={styles.forgotPassword}>
         <TouchableOpacity
-          onPress={() => navigation.navigate('ResetPasswordScreen')}
+          onPress={() => navigation.navigate("ResetPasswordScreen")}
         >
-          <Text style={styles.forgot}>Forgot your password ?</Text>
+          <Text style={styles.forgot}>Forgot your password?</Text>
         </TouchableOpacity>
       </View>
-      <Button mode="contained" onPress={onLoginPressed}>
+      <Text style={styles.alertText}>{message}</Text>
+
+      <Button mode="contained" onPress={onLoginPressed} loading={loading}>
         Log in
       </Button>
       <View style={styles.row}>
-        <Text>You do not have an account yet ?</Text> 
+        <Text>You do not have an account yet?</Text>
       </View>
       <View style={styles.row}>
-      <TouchableOpacity onPress={() => navigation.replace('RegisterScreen')}>
-          <Text style={styles.link}>Create !</Text>
+        <TouchableOpacity onPress={() => navigation.replace("RegisterScreen")}>
+          <Text style={styles.link}>Create!</Text>
         </TouchableOpacity>
       </View>
     </Background>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   forgotPassword: {
-    width: '100%',
-    alignItems: 'flex-end',
+    width: "100%",
+    alignItems: "flex-end",
     marginBottom: 10,
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 4,
   },
   forgot: {
@@ -92,7 +115,7 @@ const styles = StyleSheet.create({
     color: theme.colors.secondary,
   },
   link: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: theme.colors.primary,
   },
-})
+});
